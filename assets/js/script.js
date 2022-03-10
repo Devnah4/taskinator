9ece9a6156f7b3fa16d1c813881507780ab5109f
Dev.Nah <mcthundercat99@gmail.com>
2022-03-09 22:25:40 -0500

    Created Basic MVP Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,44 @@
 // Button Code
-var buttonE1 = document.querySelector("save-task");
-console.log(buttonE1);
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+// Text Input
+var createFormHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  buttonEl.addEventListener("click", createTaskHandler);
+    // Check if data is empty
+    if (!taskNameInput || !taskTypeInput) {
+      alert("You Need to Fill Out the Task Form!");
+      return false;
+    }
+
+    // package up data as an object
+    var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+    };
+
+    formEl.reset();
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
+  // Creates the List Elements
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  // Hold Task Info and connects with list
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  // Adds HTML elemts to <div>
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+  // adds entire list items to list
+  tasksToDoEl.appendChild(listItemEl);
+}
+
+// Click Response
+formEl.addEventListener("submit", createFormHandler);
